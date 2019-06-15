bed9dab654145fe368033a983ef1a75042dcd012
Ivan Siderov <ivan.siderov@accedia.com>
2019-06-15 19:28:04 +0300

    added react-native-camera

--- gym101/app/components/CameraScreen/index.js
+++ gym101/app/components/CameraScreen/index.js
@@ -1,16 +1,49 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { RNCamera } from 'react-native-camera'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 
 import i18n from "@i18n"
 import { fonts } from '@config'
 
 export default class CameraScreen extends React.Component {
+
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true }
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri)
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column',}}>
         <Text style={styles.explanation}>{ i18n.t('camera-screen.explanation') }</Text>
         <Icon name="comments" size={30} color="#900" />
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
       </View>
     );
   }
@@ -23,5 +56,19 @@ const styles = StyleSheet.create({
     lineHeight: 22,
     paddingVertical: 10,
     paddingHorizontal: 10,
-  }
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
 })
