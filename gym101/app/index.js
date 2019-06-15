4600a17fbdf98529392de0f1974999905415ecd1
sideroff <ivan.sideroff@gmail.com>
2019-06-15 10:39:13 +0300

    added react-native-vector-icons

--- gym101/app/index.js
+++ gym101/app/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text style={{ fontFamily: 'TwCenMTStd-UltraBold' }}>testing</Text>
+        <Icon name="rocket" size={30} color="#900" />
       </View>
     );
   }
